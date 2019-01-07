f021a5e759a0db29f9d446ea4d6d9658f9693bf0
JONNALAGADDA Srinivas <js@ojuslabs.com>
2019-01-07 11:00:31 +0530

    Add simple functions for generating and parsing continuation tokens ...
    ... for pagination in "LIST" requests.

---
+++ ctoken_utils.go
@@ -0,0 +1,24 @@
+package goutils
+
+import (
+	"encoding/base64"
+)
+
+// EncodeCtoken accepts a string continuation token, base64-encodes it,
+// then hex-encodes it, and answers the final string.
+func EncodeCtoken(ctoken string) string {
+	buf := make([]byte, base64.StdEncoding.EncodedLen(len(ctoken)))
+	base64.StdEncoding.Encode(buf, []byte(ctoken))
+	return string(buf)
+}
+
+// DecodeCtoken converts the encoded continuation token back into its
+// original form.
+func DecodeCtoken(ctoken string) (string, error) {
+	buf := make([]byte, base64.StdEncoding.DecodedLen(len(ctoken)))
+	_, err := base64.StdEncoding.Decode(buf, []byte(ctoken))
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
